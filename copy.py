d6ce67138e96cba91a62f74ea4f0cc22bf99363a
mikelopez <Pooper@.(none)>
2013-10-12 05:02:27 -0400

    copy script working

--- copy.py
+++ copy.py
@@ -7,5 +7,5 @@ from settings import game_root, datafiles, datafile_dir
 
 for file in datafiles:
     print 'Copying %s/%s to %s/' % (game_root, file, datafile_dir)
-    os.system('cp %s/%s %s/' % (game_root, file, datafile_dir))
+    os.system('cp "%s/%s" %s/' % (game_root, file, datafile_dir))
 
